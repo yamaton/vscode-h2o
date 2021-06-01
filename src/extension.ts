c57e78d7c59d115f4350aa7391e3d481dc8d0208
Yamato Matsuoka <yamato.matsuoka@tetradresearch.com>
2021-06-01 14:02:28 -0400

    Make basic completions work

--- src/extension.ts
+++ src/extension.ts
@@ -18,47 +18,26 @@ export async function activate(context: vscode.ExtensionContext) {
         simpleOldTypeCompletion.documentation = new vscode.MarkdownString('I hate this old style');
         simpleOldTypeCompletion.detail = 'show me a detail!';
 
-        const simpleCompletion = new vscode.CompletionItem('--help');
-        simpleCompletion.documentation = new vscode.MarkdownString('here comes help!');
+        const simpleCompletion = new vscode.CompletionItem('--nanachi');
+        simpleCompletion.documentation = new vscode.MarkdownString('here comes nanachi!');
 
         const snippetCompletion = new vscode.CompletionItem('--option-with-arg');
         snippetCompletion.insertText = new vscode.SnippetString('--option-with-arg ${1:<arg>}');
         snippetCompletion.documentation = new vscode.MarkdownString('An option with argument.');
 
         const tree = parser.parse(document.getText());
-        const compSubcommands = getCompSubcommands(tree.rootNode, position, fetcher);
-        const cmdNodeEnd = _getCommandNode(tree.rootNode, position)?.endPosition;
-        console.log('position: ', position);
-        console.log('cmdNodeEnd: ', cmdNodeEnd);
 
-        //   const result: vscode.CompletionItem[] = [];
-
-        //   const currentRange = document.getWordRangeAtPosition(position);
-        //   if (currentRange !== undefined) {
-        //     const rangeWithPrefix = new vscode.Range(
-        //       currentRange?.start.translate(0, -2),
-        //       currentRange?.end
-        //     );
-        //     const tokenWithPrefix = document.getText(rangeWithPrefix);
-        //     if (tokenWithPrefix.slice(0, 2) === ' -') {
-        //       result.push(simpleCompletion);
-        //       result.push(snippetCompletion);
-        //       result.push(simpleOldTypeCompletion);
-        //     } else if (tokenWithPrefix.slice(0, 2) === '--') {
-        //       result.push(simpleCompletion);
-        //       result.push(snippetCompletion);
-        //     }
-        //   }
-        //   return result;
-        // }
+        // this is a trick to get current Node
+        const p = walkbackIfNeeded(tree.rootNode, position);
+        const compSubcommands = getCompletionsSubcommands(tree.rootNode, p, fetcher);
+        const compOptions = getCompletionsOptions(tree.rootNode, p, fetcher);
 
         const res = [
           simpleCompletion,
           simpleOldTypeCompletion,
           snippetCompletion
         ];
-        const items = getCompSubcommands(tree.rootNode, position, fetcher);
-        res.push(...items);
+        res.push(...compSubcommands, ...compOptions);
         return res;
       }
     },
@@ -70,15 +49,15 @@ export async function activate(context: vscode.ExtensionContext) {
 
       const content = document.getText();
       const tree = parser.parse(content);
-      const thisName = findNode(tree.rootNode, position).text!;
-      console.log("findNode(tree.rootNode, position): ", findNode(tree.rootNode, position));
-      const cmd = getMatchingCommand(tree.rootNode, position, fetcher);
+      const thisName = getCurrentNode(tree.rootNode, position).text!;
+      console.log("findNode(tree.rootNode, position): ", getCurrentNode(tree.rootNode, position));
+      const cmd = getMachingCommand(tree.rootNode, position, fetcher);
       const subcmd = getMatchingSubcommand(tree.rootNode, position, fetcher);
       const opt = getMatchingOption(tree.rootNode, position, fetcher);
       if (cmd) {
         return new vscode.Hover(new vscode.MarkdownString(cmd.description!));
       } else if (subcmd) {
-        const cmdName = getCommandName(tree.rootNode, position)!;
+        const cmdName = getContextCommandName(tree.rootNode, position)!;
         const msg = `${cmdName} **${subcmd.name}**: ${subcmd.description}`;
         return new vscode.Hover(new vscode.MarkdownString(msg));
       } else if (opt) {
@@ -93,47 +72,7 @@ export async function activate(context: vscode.ExtensionContext) {
 }
 
 
-function getMatchingCommand(root: SyntaxNode, position: vscode.Position, fetcher: CachingFetcher): undefined | Command {
-  const cmdName = getCommandName(root, position);
-  const thisName = findNode(root, position)?.text;
-  console.log('cmdName: ', cmdName);
-  console.log('thisName: ', thisName);
-  if (cmdName === thisName) {
-    const command = fetcher.fetch(cmdName)!;
-    return command;
-  }
-}
-
-function getMatchingSubcommand(root: SyntaxNode, position: vscode.Position, fetcher: CachingFetcher): undefined | Command {
-  const cmdName = getCommandName(root, position)!;
-  const subName = getSubcommand(root, position, fetcher)!;
-  const thisName = findNode(root, position)?.text;
-  console.log('cmdName: ', cmdName);
-  console.log('subName: ', subName);
-  console.log('thisName: ', thisName);
-  if (subName === thisName) {
-    const command = fetcher.fetch(cmdName)!;
-    const subcmd = command?.subcommands?.find((x) => x.name === subName)!;
-    return subcmd;
-  }
-}
-
-function getMatchingOption(root: SyntaxNode, position: vscode.Position, fetcher: CachingFetcher): undefined | Option {
-  const thisName = findNode(root, position).text!;
-  if (thisName.startsWith('-')) {
-    const cmdName = getCommandName(root, position)!;
-    const command = fetcher.fetch(cmdName)!;
-    const subName = getSubcommand(root, position, fetcher);
-    let options: Option[];
-    if (subName) {
-      options = command?.subcommands?.find((x) => x.name === subName)?.options!;
-    } else {
-      options = command?.options!;
-    }
-    const theOption = options.find((x) => x.names.includes(thisName))!;
-    return theOption;
-  }
-}
+// --------------- Helper ----------------------
 
 // Borrow from bash-language-server
 async function initializeParser(): Promise<Parser> {
@@ -154,10 +93,91 @@ function range(n: SyntaxNode): vscode.Range {
   );
 }
 
-// Get command name if applicable
-function getCommandName(root: SyntaxNode, position: vscode.Position): string | undefined {
+
+// --------------- For Hovers and Completions ----------------------
+
+// Find the deepest node that contains the position in its range.
+function getCurrentNode(n: SyntaxNode, position: vscode.Position): SyntaxNode {
+  if (!(range(n).contains(position))) {
+    console.error("Out of range!");
+  }
+  for (const child of n.children) {
+    const r = range(child);
+    if (r.contains(position)) {
+      return getCurrentNode(child, position);
+    }
+  }
+  return n;
+}
+
+
+// Moves the position left by one character IF position is contained only in the root-node range.
+// This is just a workround as you cannot reach command node if you start from
+// the position, say, after 'echo '
+function walkbackIfNeeded(n: SyntaxNode, position: vscode.Position): vscode.Position {
+  for (const child of n.children) {
+    const r = range(child);
+    if (r.contains(position)) {
+      return position;
+    }
+  }
+  // move the position back if you cannot go deeper from the root node
+  if (n.type === 'program' && position.character > 0) {
+    return position.translate(0, -1);
+  }
+  return position;
+}
+
+
+// Returns current word as a command if the tree-sitter says it's command
+function getMachingCommand(root: SyntaxNode, position: vscode.Position, fetcher: CachingFetcher): undefined | Command {
+  const cmdName = getContextCommandName(root, position);
+  const thisName = getCurrentNode(root, position)?.text;
+  console.log('cmdName: ', cmdName);
+  console.log('thisName: ', thisName);
+  if (cmdName === thisName) {
+    const command = fetcher.fetch(cmdName)!;
+    return command;
+  }
+}
+
+
+// Returns current word as a subcommand if the tree-sitter says so
+function getMatchingSubcommand(root: SyntaxNode, position: vscode.Position, fetcher: CachingFetcher): undefined | Command {
+  const [cmd, subcmd] = getContextCmdSubcmdPair(root, position, fetcher);
+  const currentNodeText = getCurrentNode(root, position)?.text;
+  console.log('cmdName: ', cmd?.name);
+  console.log('subName: ', subcmd?.name);
+  console.log('currentNodeText: ', currentNodeText);
+  if (cmd && subcmd && subcmd.name === currentNodeText) {
+    return subcmd;
+  }
+}
+
+
+// Returns current word as an option if the tree-sitter says so
+function getMatchingOption(root: SyntaxNode, position: vscode.Position, fetcher: CachingFetcher): undefined | Option {
+  const thisName = getCurrentNode(root, position).text!;
+  if (thisName.startsWith('-')) {
+    const [cmd, subcmd] = getContextCmdSubcmdPair(root, position, fetcher);
+    if (cmd) {
+      let options: Option[];
+      if (subcmd) {
+        options = subcmd.options;
+      } else {
+        options = cmd?.options;
+      }
+      const theOption = options.find((x) => x.names.includes(thisName))!;
+      return theOption;
+    }
+  }
+}
+
+
+// Get command name covering the position if exists
+function getContextCommandName(root: SyntaxNode, position: vscode.Position): string | undefined {
   // if you are at a command, a named node, the currentNode becomes one-layer deeper than other nameless nodes.
-  const commandNode = _getCommandNode(root, position);
+  const commandNode = _getContextCommandNode(root, position);
   let name = commandNode?.firstNamedChild?.text!;
   if (name === 'sudo') {
     name = commandNode?.firstNamedChild?.nextSibling?.text!;
@@ -167,38 +187,48 @@ function getCommandName(root: SyntaxNode, position: vscode.Position): string | u
 
 // Get subcommand name if applicable
 // [FIXME] this catches option's argument; use database instead
-function* _getSubcommandCandidates(root: SyntaxNode, position: vscode.Position) {
-  let commandNode = _getCommandNode(root, position)!;
+function _getSubcommandCandidates(root: SyntaxNode, position: vscode.Position) {
+  const candidates: string[] = [];
+  let commandNode = _getContextCommandNode(root, position)!;
   if (commandNode) {
     let n = commandNode?.firstNamedChild;
     while (n?.nextSibling) {
       n = n?.nextSibling;
       if (!n.text.startsWith('-')) {
-        yield n.text;
+        candidates.push(n.text);
       }
     }
   }
+  return candidates;
 }
 
-function getSubcommand(root: SyntaxNode, position: vscode.Position, fetcher: CachingFetcher) {
-  let name = getCommandName(root, position);
-  if (name) {
-    let command = fetcher.fetch(name);
-    let subnames = command?.subcommands?.map((x) => x.name);
-    if (subnames) {
-      let gen = _getSubcommandCandidates(root, position);
-      for (let candidate of gen) {
-        if (candidate && subnames.includes(candidate)) {
-          return candidate;
+
+// Get command and subcommand inferred from the current position
+function getContextCmdSubcmdPair(root: SyntaxNode, position: vscode.Position, fetcher: CachingFetcher): [Command | undefined, Command | undefined] {
+  let name = getContextCommandName(root, position);
+  if (!name) {
+    return [undefined, undefined];
+  }
+
+  let command = fetcher.fetch(name);
+  let subcommands = command?.subcommands;
+  if (subcommands && subcommands.length) {
+    let words = _getSubcommandCandidates(root, position);
+    for (let word of words) {
+      for (const subcmd of subcommands) {
+        if (subcmd.name === word) {
+          return [command, subcmd];
         }
       }
     }
   }
+  return [command, undefined];
 }
 
 
-function _getCommandNode(root: SyntaxNode, position: vscode.Position): SyntaxNode | undefined {
-  let currentNode = findNode(root, position);
+// Get command node inferred from the current position
+function _getContextCommandNode(root: SyntaxNode, position: vscode.Position): SyntaxNode | undefined {
+  let currentNode = getCurrentNode(root, position);
   if (currentNode.parent?.type === 'command_name') {
     currentNode = currentNode.parent;
   }
@@ -208,48 +238,48 @@ function _getCommandNode(root: SyntaxNode, position: vscode.Position): SyntaxNod
 }
 
 
-// Find the deepest node in which that the position in contained.
-function findNode(n: SyntaxNode, position: vscode.Position): SyntaxNode {
-  if (!(range(n).contains(position))) {
-    console.error("Out of range!");
-  }
-  for (const child of n.children) {
-    const r = range(child);
-    if (r.contains(position)) {
-      return findNode(child, position);
-    }
-  }
-  return n;
-}
-
-
-//
-function _getSubcommands(name: string, fetcher: CachingFetcher): Command[] {
-  const subcommands = fetcher.fetch(name)?.subcommands;
-  if (subcommands) {
-    return subcommands;
-  }
-  return [];
-}
-
 // Get subcommand completions
-function getCompSubcommands(root: SyntaxNode, position: vscode.Position, fetcher: CachingFetcher): vscode.CompletionItem[] {
-  const cmd = getCommandName(root, position);
-  if (cmd) {
-    const subname = getSubcommand(root, position, fetcher);
-    if (subname === undefined) {
-      const subcommands = _getSubcommands(cmd, fetcher);
-      if (subcommands && subcommands.length) {
-        const compitems = subcommands.map((sub) => {
-          const item = new vscode.CompletionItem(sub.name);
-          item.documentation = new vscode.MarkdownString(sub.description);
-          return item;
-        });
-        return compitems;
-      }
+function getCompletionsSubcommands(root: SyntaxNode, position: vscode.Position, fetcher: CachingFetcher): vscode.CompletionItem[] {
+  const [cmd, subcmd] = getContextCmdSubcmdPair(root, position, fetcher);
+  if (cmd && subcmd === undefined) {
+    const subcommands = cmd.subcommands;
+    if (subcommands && subcommands.length) {
+      const compitems = subcommands.map((sub) => {
+        const item = new vscode.CompletionItem(sub.name);
+        item.documentation = new vscode.MarkdownString(sub.description);
+        return item;
+      });
+      return compitems;
     }
   }
   return [];
+}
+
+
+// Get option completion
+function getCompletionsOptions(root: SyntaxNode, position: vscode.Position, fetcher: CachingFetcher): vscode.CompletionItem[] {
+  const [cmd, subcmd] = getContextCmdSubcmdPair(root, position, fetcher);
+  const compitems: vscode.CompletionItem[] = [];
+  if (cmd) {
+    let options: Option[];
+    if (subcmd) {
+      options = subcmd.options;
+    } else {
+      options = cmd?.options;
+    }
+    options.forEach(opt => {
+      opt.names.forEach(name => {
+        const item = new vscode.CompletionItem(name);
+        item.documentation = new vscode.MarkdownString(opt.description);
+        if (opt.argument) {
+          const snippet = `${name} \$\{1:${opt.argument}\}`;
+          item.insertText = new vscode.SnippetString(snippet);
+        }
+        compitems.push(item);
+      });
+    });
+  }
+  return compitems;
 }
 
 
